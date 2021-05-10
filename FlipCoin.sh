79a0ced311222b8f0e7f92e9c47acb1f78d01b5e
Milind_77 <milinddhote77@gmail.com>
2021-05-10 16:14:46 +0530

    Added: UC-2_Loop_Coin_To_Show_Numberoftimes_Head_Tails_As_Winner

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -1,1 +1,19 @@
-
+#!/bin/bash
+#UserCase_2
+echo "Welcome to flip coin simulator"
+hcount=0
+tcount=0
+for count in {1..10..1}
+do
+	rn=$((RANDOM%2))
+	if [ $rn -eq 0 ]
+	then
+		echo "Tails"
+		tcount=$(($tcount+1))
+	else
+		echo "Heads"
+		hcount=$(($hcount+1))
+	fi
+done
+echo "Heads Won : $hcount"
+echo "Tails Won : $tcount"
